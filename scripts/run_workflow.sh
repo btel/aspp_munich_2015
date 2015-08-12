5f638bd892c4e5966758cf76653b69fcd466642f
Bartosz T <bartosz@telenczuk.pl>
2015-08-13 01:03:32 +0200

    create non-existing output directories

--- scripts/run_workflow.sh
+++ scripts/run_workflow.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p ../figures ../results
+
 python batch_analyse.py ../retina/Data/data_02*.mat --dest-dir ../results
 
 python merge_script.py ../results/data_02_*.npz ../results/merged_data.npz
