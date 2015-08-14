0ff0e90711b23fa4bd61d4ff6e4a1202dceb81e2
Bartosz T <bartosz@telenczuk.pl>
2015-08-14 02:37:29 +0200

    updated paths

--- workflows/run_workflow.sh
+++ workflows/run_workflow.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 mkdir -p ../figures ../results
+SCRIPTS=../scripts
 
-python batch_analyse.py ../retina/Data/data_02*.mat --dest-dir ../results
+cd $SCRIPTS
+
+python batch_analyse.py ../data/retina/data_02*.mat --dest-dir ../results
 
 python merge_script.py ../results/data_02_*.npz ../results/merged_data.npz
 
