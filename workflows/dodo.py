f7c55e8e590241d4930f8763a757c9f6745e430c
rob <rob.modini@epfl.ch>
2015-09-02 18:29:12 +0200

    extended to plot

--- workflows/dodo.py
+++ workflows/dodo.py
@@ -11,24 +11,32 @@ def mk_targetname(p):
     core, ext = os.path.splitext(filename)
     filename = core + '.npz'
     return os.path.join('../results', filename)
-    
+
+def mk_plottargetname(p):
+    directory, filename = os.path.split(p)
+    core, ext = os.path.splitext(filename)
+    filename = core + '.png'
+    return os.path.join('../results', filename)
 
 src = glob.glob('../data/data_02*.mat')
 targets = list(map(mk_targetname, src))
+plot_targets = list(map(mk_plottargetname, src))
 
 def task_open_data():
     for inp, out in zip(src, targets):
         yield {
-            'actions' : ['python open_data.py %(dependencies)s --save %(targets)s'],
+            'actions' : ['python ../scripts/open_data.py %(dependencies)s --save %(targets)s'],
             'file_dep' : [inp],
             'targets' : [out],
             'name' : os.path.split(inp)[1]
             }
 
 def task_plot_correlations():
-    return {
-        'actions' : ['python plot_correlations_histogram.py %(dependencies)s --save %(targets)s'],
-        'file_dep' : ['../results/correlations.npz'],
-        'targets' : ['../figures/correlations.png']
-        }
+    for inp, out in zip(targets, plot_targets):
+        yield {
+            'actions' : ['python ../scripts/plot_correlations_histogram.py %(dependencies)s --save %(targets)s'],
+            'file_dep' : [inp],
+            'targets' : [out],
+            'name' : os.path.split(inp)[1]
+            }
 
